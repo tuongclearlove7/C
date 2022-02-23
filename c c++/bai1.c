bdb33b35cdfe803b05c1615cc41c23b4b71725ad
Clearlove7 <88828156+tuongclearlove7@users.noreply.github.com>
2022-02-23 09:17:45 +0700

    Update bai1.c

--- c c++/bai1.c
+++ c c++/bai1.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+/*
+Viết chương trình nhập vào các số nguyên: a, b, x, y, ... 
+sau đó in ra màn hình kết quả của các biểu thức sau:
+a/ x+y/2+x/y
+b/ (a+4)(b-2c+3)/r/2h-9(a-1)
+c/ x^y, x > 0
+ */
 int main() 
 {
 	int a,b;
