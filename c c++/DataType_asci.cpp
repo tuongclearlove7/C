e0811c3f2c0365525c05415e39a639c33ed5839d
Clearlove7 <88828156+tuongclearlove7@users.noreply.github.com>
2022-05-10 14:34:09 +0700

    Create DataType_asci.cpp

---
+++ c c++/DataType_asci.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    for(int i=0 ; i <= 255 ; i++)
+        cout << "ASCII NUMBER : " << i << ':' << (unsigned char)i << endl;
+    return 0;
+
+}
