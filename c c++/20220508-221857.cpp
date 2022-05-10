47949fb6a4fde606246d5a9a78f1d14fdb579c85
Clearlove7 <88828156+tuongclearlove7@users.noreply.github.com>
2022-05-10 14:42:44 +0700

    Add files via upload

---
+++ c c++/20220508-221857.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+int main() {
+    int i=0;
+    while(i<=99) {
+        i+=1;
+        if(i%11==0){
+            std::cout<<"\n";
+        }
+       std::cout<<i<<", ";
+    }
+}
+
+
+
+
+
+
