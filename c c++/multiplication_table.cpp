d1b0113801bb37fb3fccad5ab1fece962e621b54
Clearlove7 <88828156+tuongclearlove7@users.noreply.github.com>
2022-05-10 14:35:58 +0700

    Create multiplication_table.cpp

---
+++ c c++/multiplication_table.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+   for(int i=2;i<= 9;i++){
+       for(int j=1; j<= 10;j++) {
+           std::cout<<i;
+           std::cout<<'x';
+           std::cout<<j;
+           std::cout<<'=';
+           std::cout<<i*j;
+           std::cout<<'\n';
+}
+    }
+        }
