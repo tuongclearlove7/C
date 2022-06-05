d0e0a6f1da6b6905848af811c29b9c84a0c20c2e
Clearlove7 <88828156+tuongclearlove7@users.noreply.github.com>
2022-06-05 14:59:53 +0700

    Update my_file.cpp

--- c c++/Code Dev-C/file/my_file.cpp
+++ c c++/Code Dev-C/file/my_file.cpp
@@ -4,10 +4,18 @@
 #include <conio.h>
 #include <iomanip>
 #include<algorithm>
-
 #include <fstream> 
 #include<process.h>
+
 using namespace std;
+struct Student{
+	char name[100];
+	float hoa;
+	float ly;
+	float toan;
+	float tb;
+	int masv;
+};
 
 int Count_Char_Upper(char s[]) {
  int i=0;
@@ -137,17 +145,77 @@ handle(int arr[], long int n){
 	outfile <<"\n"<< i;
 	outfile.close();
 }
+Student handle_student(Student arr[],int n){
+	ofstream fstudent; 
+	fstudent.open("Sinhvien.txt"); 
+	int j;
+	int key[4] = {101,102,103,104};
+	cout<<"| nhap so sinh vien : ";
+	std::cin>>n;
+		for(int i=1;i<=n;i++) {
+			cout<<"| nhap ten : ";
+			cin>>arr[i].name;
+			cout<<"| vui long nhap ma sinh vien : ";
+			cin>>arr[i].masv;
+			do {
+			if (arr[i].masv!=key[0] && arr[i].masv!=key[1] && arr[i].masv!=key[2] && arr[i].masv!=key[3])
+				    cout<<"| Ma sinh vien nay k ton tai vui long nhap lai!!! : ";
+				    cin>>arr[i].masv;	
+		    }while(arr[i].masv!=key[0] && arr[i].masv!=key[1] && arr[i].masv!=key[2] && arr[i].masv!=key[3]); 
+		   		cout<<"| chuc mung ban da nhap dung ma sinh vien bam enter de tiep tuc\n";
+			   	cin.ignore(256, '\n');
+			   	cout<<"| ";
+			   	cin.ignore(256, '\n');
+			   	
+			   		cout<<"| nhap  toan : ";
+					cin>>arr[i].toan;
+					cout<<"| nhap diem hoa : ";
+					cin>>arr[i].hoa;
+					cout<<"| nhap diem ly : ";
+					cin>>arr[i].ly;
+	}
+	
+	cout<<"| nhap so hoc sinh can xem : ";
+	cin>>n;
+	for(int i=1;i<=n;i++){
+		cout<<"\n_________________________________________________________\n";
+		fstudent<<"\n_________________________________________________________\n";
+		fstudent<<"| "<<i<<". sinh vien "<<strupr(arr[i].name);
+		fstudent<<"\n| ma sinh vien cua sinh vien "<<arr[i].name<<" la : "<<arr[i].masv;
+		fstudent<<"\n| diem toan cua "<<arr[i].name<<" la : "<<arr[i].toan;
+		fstudent<<"\n| diem hoa cua "<<arr[i].name<<" la : "<<arr[i].hoa;
+		fstudent<<"\n| diem ly cua "<<arr[i].name<<" la : "<<arr[i].ly;
+		arr[i].tb=(float) (arr[i].toan + arr[i].ly + arr[i].hoa)/3;
+		fstudent<<"\n| diem trung binh cua sinh vien "<<arr[i].name<<" la : "<<arr[i].tb<<"\n";
+		
+		
+		cout<<"| "<<i<<". sinh vien "<<strupr(arr[i].name);
+		cout<<"\n| ma sinh vien cua sinh vien "<<arr[i].name<<" la : "<<arr[i].masv;
+		cout<<"\n| diem toan cua "<<arr[i].name<<" la : "<<arr[i].toan;
+		cout<<"\n| diem hoa cua "<<arr[i].name<<" la : "<<arr[i].hoa;
+		cout<<"\n| diem ly cua "<<arr[i].name<<" la : "<<arr[i].ly;
+		arr[i].tb=(float) (arr[i].toan + arr[i].ly + arr[i].hoa)/3;
+		cout<<"\n| diem trung binh cua sinh vien "<<arr[i].name<<" la : "<<arr[i].tb<<"\n";
+		
+	}
+	fstudent.close();
+}
+Student HAND(){
+	Student arr[100];
+	Student rank;
+	int n;
+	handle_student(arr,n);
 
+}
 int main() {
 	int choose=-1;
 	ofstream sort_file; 
 	int n; 
 	int arr[10000];
 	cout << "====================Menu====================" << endl;
-	//handle(arr,n);
 	cout << setw(15) << right <<""<<"Bai tap ve file\n";
-	cout<<"1 : bai 1, bai 2, bai 3, bai 4, bai 5, bai 6, bai 8, bai 10.\n2 : bai 7\n";
-	cout << setw(10) << right <<""<<"Options!!! [keywords] : 1, 2 \n";
+	cout<<"1 : bai 1, bai 2, bai 3, bai 4, bai 5, bai 6, bai 8, bai 10.\n2 : bai 7\n3 : bai 9\n";
+	cout << setw(10) << right <<""<<"Options!!! [keywords] : 1, 2, 3 \n";
 	cout << setw(15) << right <<""<<"choose program : ";
 	cin>>choose;
 	cin.ignore(256,'\n');
@@ -159,11 +227,15 @@ int main() {
 			case 2:
 			soft_num_infile();
     		break;
+    		case 3:
+    			HAND();
+    		break;
 			}
 		break;
 	}
 	cout << "\n====================END=====================" << endl;
 }
+
 
 
 
