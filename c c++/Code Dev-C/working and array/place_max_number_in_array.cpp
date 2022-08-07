5b67673c5d0be9986af913cab168f999f86d12cb
Clearlove7 <88828156+tuongclearlove7@users.noreply.github.com>
2022-08-07 23:46:31 +0700

    Update code
    commit update code

--- c c++/Code Dev-C/working and array/place_max_number_in_array.cpp
+++ c c++/Code Dev-C/working and array/place_max_number_in_array.cpp
@@ -22,14 +22,49 @@ int place_min(int arr[],int n){//tim vi tri mang chua so nho nhat
 	}
 	return flag;
 }
+int static_place_maxarr(){
+	int arr[5]={1,2,3,4,5};
+	int flag = 0;
+	for(int i = 0;i<5;i++){
+		if(arr[i]>arr[flag])
+		flag = i;
+	}
+	return flag;
+}
+int static_place_minarr(){
+	int arr[5]={1,2,3,4,5};
+	int flag = 0;
+	for(int i = 0;i<5;i++){
+		if(arr[i]<arr[flag])
+		flag = i;
+	}
+	return flag;
+}
 int main(){
- int arr[100];
- int n;
- std::cout<<"nhap n : ";
- cin>>n;
- input(arr,n);
- std::cout<<"vi tri mang chua so nho nhat la : "<<place_min(arr,n);
- std::cout<<endl;
- std::cout<<"vi tri mang chua so lon nhat la : "<<place_max(arr,n);
+	int arr[100];
+	int n;
+	std::cout<<"nhap n : ";
+	cin>>n;
+	input(arr,n);
+	std::cout<<"vi tri mang chua so nho nhat la : "<<place_min(arr,n);
+	std::cout<<endl;
+	std::cout<<"vi tri mang chua so lon nhat la : "<<place_max(arr,n);
+	std::cout<<endl;
+	std::cout<<"vi tri mang chua so lon nhat trong mang static la : "<<static_place_maxarr();
+	std::cout<<endl;
+	std::cout<<"vi tri mang chua so nho nhat trong mang static la : "<<static_place_minarr();
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
