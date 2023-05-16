c7f8636bc59e6dd964757a5c22215b9049eb185b
Clearlove7 <88828156+tuongclearlove7@users.noreply.github.com>
2023-05-16 16:54:16 +0700

    Update hoan_vi_lap.cpp

--- c c++/Code Dev-C/toan roi rac/hoan_vi_lap.cpp
+++ c c++/Code Dev-C/toan roi rac/hoan_vi_lap.cpp
@@ -6,22 +6,23 @@ int b[20], k=3, n=4;
 void print(int k){
 	
 	for(int i = 1; i<=k; i++){
-		cout<<b[i]<<" ";
+		cout<<b[i];
 	}
+	cout<<endl;
 	
 }
 
 void hv_lap(int n,int k){
 	
-	for(int i = 1; i<=k;i++)b[i] = 0;
+	for(int i = 1; i <= k; i++) b[i] = 1;
 	print(k);
-	
-	for(int c = 1; c < pow(n,k); c++){
+	for(int c = 1; c < pow(n,k); c++) {
 		int i = k;
-		while(b[i]==n)i=i-1;
-		b[i]=b[i]+1;
-		for(int j = i+1; j <= k; j++)b[j]=0;
-		print(k);
+		while(b[i] == n) i = i - 1;
+		b[i] = b[i] + 1;
+		for(int j = i + 1; j <= k; j=j+1) 
+			b[j] = 1;
+			print(k);
 	}
 }
 
