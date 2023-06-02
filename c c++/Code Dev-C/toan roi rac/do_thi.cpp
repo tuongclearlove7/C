f9207ca409e606c8297315066dd637b10f4c3de3
Clearlove7 <88828156+tuongclearlove7@users.noreply.github.com>
2023-06-02 16:54:54 +0700

    Update do_thi.cpp

--- c c++/Code Dev-C/toan roi rac/do_thi.cpp
+++ c c++/Code Dev-C/toan roi rac/do_thi.cpp
@@ -5,7 +5,27 @@ int G[20][20], n, C[20], u=1;
 
 FILE *fptr;
 
-
+void Vetext(int G[20][20], int V_in[20], int V_out [20], int n)
+{
+       printf("\n Nua bac ra: \n");
+       for(int i=1;i<=n;i++) {
+             V_out[i] = 0;
+              for(int j =1 ;j<=n; j++) V_out[i] +-6[i][j];
+              V_out[i] +-G[i][i];
+              printf("deg_out (%d)=%d\t",i,V_out[i]);
+       }
+       printf("\n Nua bac vao: \n");
+       for(int j =1 ;j<=n;j++){
+              V_in[j] = 0;
+              for(int i=1;i<=n;i++) V_in[j] +-G[i][j];
+              V_in[i]+=G[i][i];
+              printf("deg_out (%d)=%d\t,j,V_in[j]);
+       }
+}
+		     
+	
+		     
+		     
 void view_graph(int G[20] [20], int n){
        int i,j;
        printf("so dinh %d\n",n);
@@ -17,7 +37,23 @@ void view_graph(int G[20] [20], int n){
           printf("\n\n");
       }
 }
+void DFS (int u) {
 
+      int S[20],top=1; S[top]=u;
+      while(top>0) {
+             int u=S[top];
+             top=top-1;
+             if(C[u]==1) continue;
+             printf("%d\n",u);
+             C[u]=1;
+       for (int v=n;v>=1;v--)
+             if(G[u][v] -0 && C[v]==0) {
+                   top-top+1;
+                  S[top]=v;
+
+             }
+       }
+}
 
 int init(char * path){
 	
