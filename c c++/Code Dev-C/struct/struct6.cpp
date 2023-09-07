e17bfe2524b5ff7a297a684d2ba3fbf623763b07
Clearlove7 <88828156+tuongclearlove7@users.noreply.github.com>
2023-09-07 16:59:51 +0700

    Create struct6.cpp

---
+++ c c++/Code Dev-C/struct/struct6.cpp
@@ -0,0 +1,84 @@
+#include<bits/stdc++.h>
+#include <iostream>
+
+int tim_x_trong_mang(int a[], int x){
+	
+	
+}
+
+
+//cho 1 mang a gom n phan tu va 1 mang b gom m phan tu 
+//(ca 2 mang deu da duoc sap xep tang dan).
+//Hay tao thanh mang c la gop cua 2 mang a va b
+// sao cho mang c van duoc sap xep tang dan (k duoc sort lai mang c) 
+void ket_hop_mang_va_sap_xep(int a[], int b[], int c[]){
+	
+	for(int i = 0; i<10;++i){
+		for(int j =i; j<10;++j){
+			if(a[i]>a[j]){
+				int t = a[i];
+				a[i] = a[j];
+				a[j] = t;
+			}
+			
+			if(b[i]>b[j]){
+				int t = b[i];
+				b[i] = b[j];
+				b[j] = t;
+			}
+		}
+	}
+	
+	for(int i = 1; i<10;++i){
+	
+		std::cout<<a[i];
+	}
+	std::cout<<std::endl;
+	
+	
+	for(int i = 1; i<10;++i){
+	
+		std::cout<<b[i];
+	}
+	std::cout<<std::endl;
+	
+	
+	
+	return;
+}
+
+int binary_search(int a[], int x){
+	
+	
+	    int left = 0;
+        int right =10;
+
+        for (int i = left; i <= right; i++){
+            
+            int mid = (left + right)/2;
+            
+            if(a[mid] == x){
+                return mid;
+            }else if(a[mid] > x){
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return 0;
+}
+
+main() {
+	
+	int a[10] = {1,2,3,4,5,6,7,9,8};
+	int b[10] = {1,2,3,4,5,6,7,9,11};
+	int c[10];
+
+	std::cout<<binary_search(a,3)<<std::endl;
+	
+	ket_hop_mang_va_sap_xep(a,b,c);
+	
+
+
+	
+}
