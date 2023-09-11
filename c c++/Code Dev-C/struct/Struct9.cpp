bb05aa64cca07d1512d93bb00ab669dc77ac0598
Clearlove7 <88828156+tuongclearlove7@users.noreply.github.com>
2023-09-11 16:57:57 +0700

    Update Struct9.cpp

--- c c++/Code Dev-C/struct/Struct9.cpp
+++ c c++/Code Dev-C/struct/Struct9.cpp
@@ -1,43 +1,42 @@
-Viết chương trình nhập vào một dãy số nguyên a1, a2, ..., an. Tìm độ dài dãy con tăng dần dài nhất. (Dãy con là dãy các phần tử liên tiếp nhau trong mảng).
-#include<iostream>
-using namespace std; 
+#include<bits/stdc++.h>
+#include <iostream>
 
-void printLongestDistance(int a[],int n){
-	int maxLength; // day con dai nhat. 
-	int currentLength=1; // do dai dai con hien tai
-	int endIndex=0; // chi so cuoi cung cua day con dai nhat. 
+//Viết chương trình nhập vào một dãy số nguyên a1, a2, ..., an.
+// Tìm độ dài dãy con tăng dần dài nhất. 
+//(Dãy con là dãy các phần tử liên tiếp nhau trong mảng).
+
+
+int tim_do_dai_day_con(int a[], int n){
 	
+		int maxLength=0;  
+		int currentLength=1; 
+		int endIndex=0; 
 	
-	for(int i=1;i<n;i++){
-		if(a[i]>a[i-1]){
-			currentLength++;
-			if(currentLength >maxLength){
-				maxLength=currentLength;
-				endIndex=i;
+		for(int i=1;i<n;i++){
+			if(a[i]>a[i-1]){
+				currentLength++;
+				if(currentLength >maxLength){
+					maxLength=currentLength;
+					endIndex=i;
+				}
+			}else{
+				currentLength=1;
 			}
-		}else{
-			currentLength=1;
 		}
-	}
-	cout<<"Do dai lon nhat cua mang la: "<<maxLength<<endl;
-	cout<<"cac phan tu trong khoang do dai lai: "<<endl;
-		for(int i=endIndex-maxLength+1;i<=endIndex;i++){
-			cout<<a[i]<<" ";
-		}
-		cout<<endl;
+		std::cout<<"Do dai day con dai nhat : "<<maxLength<<std::endl;		
+		
+		return 0;	
 }
 
-int main(){
-	int n;
-	cout<<"Nhap do dai cua mang : ";
-	cin>>n;
+int main() {
 	
-	int a[n];
-	for(int i=0;i<n;i++){
-		cout<<"nhap mang vi tri thu  "<<i+1<<" : ";
-		cin>>a[i];
-	}
-	printLongestDistance(a,n);
+	int a[10] = {1,2,3,9,5,6,7,9,8};
 	
-return 0;	
+	tim_do_dai_day_con(a, 10);
+	
+	
+	
+
+
+	
 }
